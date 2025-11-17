ac096313be29c43b06be79ae3864c0267f5b9240
Mikhail <>
2025-11-17 20:13:36 +0300

    - делаю окно со скроллом героев

---
+++ Alternativa_Games_Test/Assets/_Project/Scripts/GameScene/UI/Views/Heroes/HeroesView.cs
@@ -0,0 +1,12 @@
+using _Project.Scripts.Project.UI.Views;
+using UnityEngine;
+
+namespace _Project.Scripts.GameScene.UI.Views.Heroes
+{
+    public class HeroesView : View
+    {
+        protected override void OnInit()
+        {
+        }
+    }
+}
