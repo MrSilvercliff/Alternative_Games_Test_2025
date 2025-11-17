8eac59413bee9df36807f27068c348dc22bf8a1c
Mikhail <>
2025-11-17 22:13:41 +0300

    - делаю виджет героя

--- Alternativa_Games_Test/Assets/_Project/Scripts/GameScene/UI/Widgets/Hero/HeroWidget.cs
+++ Alternativa_Games_Test/Assets/_Project/Scripts/GameScene/UI/Widgets/Hero/HeroWidget.cs
@@ -1,12 +1,15 @@
 using _Project.Scripts.GameScene.Configs;
 using _Project.Scripts.Project.ObjectPools;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Project.Scripts.GameScene.UI.Widgets.Hero
 {
     public class HeroWidget : MonoBehaviour, IPoolable
     {
         [SerializeField] private HeroWidgetView _view;
+        [SerializeField] private Button _buttonExpand;
+        [SerializeField] private GameObject _description;
 
         private HeroData _heroData;
 
@@ -19,6 +22,7 @@ namespace _Project.Scripts.GameScene.UI.Widgets.Hero
         public void OnCreate()
         {
             gameObject.SetActive(false);
+            _buttonExpand.onClick.AddListener(OnButtonExpandClick);
         }
 
         public void OnSpawn()
@@ -27,6 +31,13 @@ namespace _Project.Scripts.GameScene.UI.Widgets.Hero
 
         public void OnDespawn()
         {
+        }
+
+        private void OnButtonExpandClick()
+        {
+            var descriptionIsActive = _description.activeInHierarchy;
+            var newActive = !descriptionIsActive;
+            _description.SetActive(newActive);
         }
     }
 }
