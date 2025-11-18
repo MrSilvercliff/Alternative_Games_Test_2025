4c25fea4e3d016ac7ef8d6d2c39984f4d93c0d27
Mikhail <>
2025-11-18 19:21:53 +0300

    - добавил моноапдейтер в ядро проекта

--- Alternativa_Games_Test/Assets/_Project/Scripts/Project/Core/ProjectCore.cs
+++ Alternativa_Games_Test/Assets/_Project/Scripts/Project/Core/ProjectCore.cs
@@ -1,4 +1,5 @@
 using _Project.Scripts.GameScene.Input;
+using _Project.Scripts.Project.MonoUpdate;
 using _Project.Scripts.Project.Singleton;
 using UnityEngine;
 
@@ -6,19 +7,23 @@ namespace _Project.Scripts.Project.Core
 {
     public interface IProjectCore
     { 
+        IMonoUpdater MonoUpdater { get; }
         IInputController InputController { get; }
     }
 
     public class ProjectCore : DontDestroyMonoBehaviourSingleton<ProjectCore>, IProjectCore
     {
+        public IMonoUpdater MonoUpdater => _monoUpdater;
         public IInputController InputController => _inputController;
 
+        [SerializeField] private MonoUpdater _monoUpdater;
         [SerializeField] private InputHandler _inputHandler;
 
         private InputController _inputController;
 
         public override bool Init()
         {
+            _monoUpdater.Init();
             InitInput();
             return true;
         }
