805e3dd77b7e8a5c22475b6da6592fb7c7fa0ccd
Mikhail <>
2025-11-19 16:14:22 +0300

    - сплющил скролл героев

--- Alternativa_Games_Test/Assets/_Project/Scripts/GameScene/UI/Widgets/Hero/HeroesScroll.cs
+++ Alternativa_Games_Test/Assets/_Project/Scripts/GameScene/UI/Widgets/Hero/HeroesScroll.cs
@@ -3,53 +3,90 @@ using _Project.Scripts.GameScene.Input;
 using _Project.Scripts.GameScene.Scene;
 using _Project.Scripts.Project.Core;
 using _Project.Scripts.Project.Extensions;
-using _Project.Scripts.Project.UI.Widgets.ScrollRects.WithSelect;
-using System;
+using _Project.Scripts.Project.Interfaces;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using ZerglingUnityPlugins.Tools.Scripts.Mono;
 
 namespace _Project.Scripts.GameScene.UI.Widgets.Hero
 {
-    public class HeroesScroll : VerticalScrollRectWithSelect<HeroData, HeroWidget>, IKeyboardInputListener
+    public class HeroesScroll : MonoBehaviour, IInitializable, IFlushable, IMonoUpdatable, IKeyboardInputListener
     {
+        public HeroWidget SelectedWidget { get; protected set; }
+        public HeroData SelectedWidgetData { get; protected set; }
+        public int SelectedIndex { get; protected set; }
+
+        protected RectTransform ScrollRectContent => _scrollRect.content;
+
+        [SerializeField] protected ScrollRect _scrollRect;
+        [SerializeField] private VerticalLayoutGroup _scrollContentLayout;
         [SerializeField] private GameObject _raycastBlocker;
+        [SerializeField] private float _lerpTMultiplier;
 
-        protected override void OnEnable()
+        protected List<HeroWidget> _widgets;
+        protected IReadOnlyList<HeroData> _widgetDatas;
+
+        protected float _widgetBasicHeight;
+
+        private Vector2 _startAnchoredPosition;
+        private Vector2 _endAnchoredPosition;
+        private float _lerpT;
+
+        protected virtual void OnEnable()
         {
-            base.OnEnable();
             ProjectCore.Instance.InputController.Subscribe(this);
+            ProjectCore.Instance.MonoUpdater.Subscribe(this);
         }
 
-        protected override void OnDisable()
+        protected virtual void OnDisable()
         {
-            base.OnDisable();
             ProjectCore.Instance.InputController.UnSubscribe(this);
+            ProjectCore.Instance.MonoUpdater.UnSubscribe(this);
         }
 
-        protected override bool OnInit()
+        public bool Init()
         {
-            _raycastBlocker.SetActive(false);
+            _widgets = new();
+
+            SelectedWidget = null;
+            SelectedWidgetData = default;
+            SelectedIndex = int.MinValue;
+
+            _startAnchoredPosition = Vector2.zero;
+            _endAnchoredPosition = Vector2.zero;
+            _lerpT = float.MaxValue;
+
             return true;
         }
 
-        protected override bool OnFlush()
+        public bool Flush()
         {
             DespawnWidgets();
             return true;
         }
 
-        protected override void OnSetup()
-        {
-            SpawnWidgets();
-        }
-
-        public override void OnUpdate()
+        public virtual void OnUpdate()
         {
             if (_raycastBlocker.activeInHierarchy)
                 return;
 
-            base.OnUpdate();
+            if (_lerpT > 1.0f)
+                return;
+
+            var deltaTime = Time.deltaTime;
+            _lerpT += deltaTime * _lerpTMultiplier;
+
+            var currentAnchoredPosition = Vector2.Lerp(_startAnchoredPosition, _endAnchoredPosition, _lerpT);
+            ScrollRectContent.anchoredPosition = currentAnchoredPosition;
+        }
+
+        public void Setup(IReadOnlyList<HeroData> widgetDataList)
+        {
+            _widgetDatas = widgetDataList;
+            SpawnWidgets();
+            SelectWidget(0);
         }
 
         private void SpawnWidgets()
@@ -80,13 +117,79 @@ namespace _Project.Scripts.GameScene.UI.Widgets.Hero
             var heroWidgetPool = GameSceneCore.Instance.ObjectPools.HeroWidgetPool;
 
             for (int i = 0; i < _widgets.Count; i++)
-            { 
+            {
                 var widget = _widgets[i];
                 widget.WidgetSelectEvent -= OnWidgetSelectEvent;
                 widget.ExpandStartEvent -= OnWidgetExpandStartEvent;
                 widget.ExpandEndEvent -= OnWidgetExpandEndEvent;
                 heroWidgetPool.Despawn(widget);
             }
+        }
+
+        public void SelectWidget(HeroWidget widget)
+        {
+            var lastSelectedWidget = SelectedWidget;
+
+            SelectedWidget = widget;
+            SelectedIndex = _widgets.IndexOf(widget);
+            SelectedWidgetData = _widgetDatas[SelectedIndex];
+
+            lastSelectedWidget?.SetSelected(false);
+            SelectedWidget.SetSelected(true);
+            ScrollToSelectedWidget();
+        }
+
+        public void SelectWidget(int index)
+        {
+            var lastSelectedWidget = SelectedWidget;
+
+            SelectedIndex = index;
+            SelectedWidget = _widgets[index];
+            SelectedWidgetData = _widgetDatas[index];
+
+            lastSelectedWidget?.SetSelected(false);
+            SelectedWidget.SetSelected(true);
+            ScrollToSelectedWidget();
+        }
+
+        public void SelectNextWidget()
+        {
+            var lastIndex = _widgets.Count - 1;
+
+            if (SelectedIndex >= lastIndex)
+                return;
+
+            var newIndex = SelectedIndex + 1;
+            SelectWidget(newIndex);
+            ScrollToSelectedWidget();
+        }
+
+        public void SelectPreviousWidget()
+        {
+            if (SelectedIndex == 0)
+                return;
+
+            var newIndex = SelectedIndex - 1;
+            SelectWidget(newIndex);
+            ScrollToSelectedWidget();
+        }
+
+        private void ScrollToSelectedWidget()
+        {
+            _lerpT = 0.0f;
+            _startAnchoredPosition = ScrollRectContent.anchoredPosition;
+
+            if (SelectedIndex == 0 || SelectedIndex == 1)
+            {
+                _endAnchoredPosition = Vector2.zero;
+                return;
+            }
+
+            var anchoredPositionTop = -(SelectedWidget.RectTransform.anchoredPosition.y + _scrollContentLayout.padding.top);
+            var offsetToCenter = _widgetBasicHeight + _scrollContentLayout.spacing;
+            var anchoredPositionY = anchoredPositionTop - offsetToCenter;
+            var anchoredPosition = new Vector2(0, anchoredPositionY);
+            _endAnchoredPosition = anchoredPosition;
         }
 
         private void OnWidgetSelectEvent(HeroWidget widget)
